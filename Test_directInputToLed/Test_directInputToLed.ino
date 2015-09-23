ad6151a7fba8f873b90f7694e467ccc066122e56
Troels Kroegh <troels.kroegh@gmail.com>
2015-09-23 11:18:31 +0200

    test stuff

---
+++ Test_directInputToLed/Test_directInputToLed.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(9, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int d = digitalRead(9);
+  digitalWrite(13, d);
+  Serial.println(d);
+  //delay(100);
+}
