58d1582b0615abc1f9e2f7946697fb92a81dc3c0
TrampeTramp <sfrand12@student.aau.dk>
2015-09-24 15:43:38 +0200

    CodeCleanup
    Bleb

--- Test_RH_Send/Test_RH_Send.ino
+++ Test_RH_Send/Test_RH_Send.ino
@@ -1,44 +1,28 @@
 #include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
+#include <SPI.h>
+#define LED_PIN 13
 
 RH_ASK driver;
-
-// Globals for transmitter
-int count = 0;
-const int led_pin = 13;
-const int transmit_pin = 12;
 
 void setup() {
   Serial.begin(9600);
 
-  // Transmitter
   if (!driver.init())
          Serial.println("init failed");
   pinMode(led_pin, OUTPUT);
-  Serial.println("Ready to send!");
-  //StartSignal();
 }
 
 void loop(){
   for(int i = 0; i < 100; i++) {
-    digitalWrite(led_pin, HIGH);
-    char buf[16] = "";
-    //itoa(count, buf, 10);
-    for(int j = strlen(buf); j < 16; j++) {
-      buf[j] = 'a' + j;
-    }
-    buf[15] = '\0';
+    digitalWrite(LED_PIN, HIGH);
+    char * buf = "abcdefghijklmno";
     driver.send((uint8_t *)buf, strlen(buf));
     driver.waitPacketSent();
-    
-    Serial.print(buf);
-    
-    Serial.println(count);
-    count++;
-    digitalWrite(led_pin, LOW);
+    digitalWrite(LED_PIN, LOW);
     delay(10);
   }
 
+  // To avoid sending more packets than we want to, halts the Arduino
   while(true);
 }
 
