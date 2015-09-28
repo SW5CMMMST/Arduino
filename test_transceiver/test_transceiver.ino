ef45f218f86372ead9097f166ef55a50c85fa1a9
Mathias Sass Michno <mathiassmichno@gmail.com>
2015-09-28 18:24:17 +0200

    upd

--- test_transceiver/test_transceiver.ino
+++ test_transceiver/test_transceiver.ino
@@ -2,13 +2,15 @@
 #include <RH_ASK.h>
 #include <SPI.h>
 #include <sass_ask.h>
+#include <Timer.h>
 
 #ifdef DEBUG
 char payloadString[64];
 #endif
 
+Timer t;
 RH_ASK driver;
-const uint8_t addr = 0x22;
+const uint8_t addr = 0x66;
 uint16_t sync = 0;
 bool h = true;
 payload_type payload;
@@ -19,42 +21,47 @@ void setup()
     if (!driver.init())
          Serial.println("init failed");
     pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
+    Serial.println("ready");
     payload.addr = addr;
     payload.sync = sync & 0x7FFF;
     payload.okay = 0x1;
-    payload.mode = PING;
-    payload.cntd = 5;
+    payload.mode = INIT;
+    payload.cntd = 0;
     payload.msga[0] = (uint8_t)'h';
     payload.msga[1] = (uint8_t)'e';
     payload.msga[2] = (uint8_t)'j';
-    delay(50);
-    digitalWrite(13, LOW);
-    Serial.println("ready");
 }
 
 void loop()
 {
+    t.update();
     if(h){
+      blink_status();
+      payload.mode = INIT;
       driver.send((uint8_t *)&payload, sizeof(payload));
+      Serial.println("initial pkg sent");
       h = false;
     }
     uint8_t len = sizeof(payload);
     if(driver.recv((uint8_t*)&payload, &len)){
-      digitalWrite(13, HIGH);
+      blink_status();
       //driver.printBuffer("Got:", (uint8_t*)&payload, 16);
       #ifdef DEBUG
       makePayloadString(payload, payloadString);
       Serial.print(payloadString);
       #endif
-      if(payload.cntd == 0)
-        payload.cntd = 10;
-      else
-        payload.cntd--;
+//      if(payload.cntd == 0)
+//        payload.cntd = 10;
+//      else
+      payload.cntd++;
+      payload.mode = PING;
       driver.send((uint8_t *)&payload, sizeof(payload));
       driver.waitPacketSent();
-      digitalWrite(13, LOW);
     }
+    
 }
 
+void blink_status(){
+  t.pulse(13, 333, LOW);  
+}
 
