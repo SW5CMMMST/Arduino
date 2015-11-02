b170b80749cb8f54634e3b8b0146989081817118
Troels Kroegh <troels.kroegh@gmail.com>
2015-11-02 11:40:54 +0100

    PJON -> RH

--- NetworkProto/NetworkProto.ino
+++ NetworkProto/NetworkProto.ino
@@ -1,34 +1,13 @@
-#include <RadioHead.h>
-#include <radio_config_Si4460.h>
-#include <RHCRC.h>
-#include <RHDatagram.h>
-#include <RHGenericDriver.h>
-#include <RHGenericSPI.h>
-#include <RHHardwareSPI.h>
-#include <RHMesh.h>
-#include <RHNRFSPIDriver.h>
-#include <RHReliableDatagram.h>
-#include <RHRouter.h>
-#include <RHSoftwareSPI.h>
-#include <RHSPIDriver.h>
-#include <RHTcpProtocol.h>
 #include <RH_ASK.h>
-#include <RH_NRF24.h>
-#include <RH_NRF51.h>
-#include <RH_NRF905.h>
-#include <RH_RF22.h>
-#include <RH_RF24.h>
-#include <RH_RF69.h>
-#include <RH_RF95.h>
-#include <RH_Serial.h>
-#include <RH_TCP.h>
+#include <SPI.h> // Not actualy used but needed to compile
 
 #include <EEPROM.h>
-#include <PJON_ASK.h>
+// #include <PJON_ASK.h>
 
 #define RX_PIN 11
 #define TX_PIN 12
 #define SLOTLENGTH 300  // Slot length in miliseconds
+#define MAX_RECEIVE_TIME SLOTLENGTH - 50
 
 typedef struct {
     uint8_t  slot;      // Current slot also doubles as count down to the empty slot
@@ -38,7 +17,9 @@ typedef struct {
 } payload_type; 
 
 // Initialize PJON library
-PJON_ASK network(RX_PIN, TX_PIN, EEPROM.read(0x00));
+//PJON_ASK network(RX_PIN, TX_PIN, EEPROM.read(0x00));
+
+RH_ASK driver;
 // Message buffers
 payload_type out_payload;
 payload_type in_payload;
@@ -48,8 +29,8 @@ int mySlot;
 int slotCount;
 uint8_t addr;
 
-// Copies the recieved message into the buffer
-static void receiver_function(uint8_t len, uint8_t *payload) {
+/* Copies the recieved message into the buffer
+void receiver_function(uint8_t len, uint8_t *payload) {
   memcpy(&in_payload, payload, len);
   Serial.print("Received Message: ");
   char buff[16];
@@ -58,7 +39,7 @@ static void receiver_function(uint8_t len, uint8_t *payload) {
     Serial.print(buff);
   }
   Serial.println();
-}
+} */
 
 // Initializes the payload to use the current data
 void makePayload(){
@@ -83,11 +64,14 @@ void connectToNetwork(){
   curSlot = mySlot;
   // Setup payload and make it ready for sending
   makePayload();
-  network.send(BROADCAST, (char*)&out_payload, sizeof(out_payload));
+  //network.send(BROADCAST, (char*)&out_payload, sizeof(out_payload));
+  
   // Wait for the empty slot
   delay((in_payload.slot - 1)*SLOTLENGTH);
+  driver.send((uint8_t*)&out_payload, sizeof(out_payload));
+  driver.waitPacketSent();
   // Send the package
-  network.update();
+  //network.update();
 }
 
 void setup() {
@@ -100,16 +84,26 @@ void setup() {
   pinMode(RX_PIN, INPUT);
   pinMode(13, OUTPUT);
   // Set the reciver function to be called by the library
-  network.set_receiver(receiver_function);
+  //network.set_receiver(receiver_function);
   // Look for 20 seconds for an existing network
   Serial.println("Looking for network...");
-  int response = network.receive(5000000L);
-  if(response == ACK){
+  //int response = network.receive(5000000L);
+  long timer = millis();
+  int response = 0;
+  payload_type buf;
+  while(timer + 10000 > millis()){
+    uint8_t len = sizeof(payload_type);
+    if(driver.recv((uint8_t*)&buf, &len)){
+      response = 1;
+      //receiver_function(len, &buf);
+      break;
+    }
+  }
+  if(response == 1){
     Serial.println("Network found!");
     // Network exsists connect to it
     connectToNetwork();
-  }
-  else{
+  } else {
     Serial.println("No network found! Creating new network.");
     // Network doesn't exists create a new
     setupNetwork();
@@ -125,12 +119,22 @@ void loop() {
   if(curSlot == mySlot){
     digitalWrite(13, HIGH);
     // Set payload for transmission
-    network.send(BROADCAST, (char*)&out_payload, sizeof(out_payload));
+    //network.send(BROADCAST, (char*)&out_payload, sizeof(out_payload));
     // Send the package
-    network.update();
+    //network.update();
+
+    driver.send((uint8_t*)&out_payload, sizeof(out_payload));
+    driver.waitPacketSent();
   } else {
     // Recieve 
-    network.receive(1000);
+    payload_type buf;
+    while(time + MAX_RECEIVE_TIME > millis()){
+      uint8_t len = sizeof(payload_type);
+      if(driver.recv((uint8_t*)&buf, &len)){
+        //receiver_function(len, buf);
+        break;
+      }
+    }
   }
   // Wait until next timeslot
   int timeleft = SLOTLENGTH-(millis()-time);
