ca5d2f1a0d3aca0e737575e60a8ba48c52426d49
Troels Krøgh <troels.kroegh@gmail.com>
2015-11-18 20:18:07 +0100

    removed typedef to avoid obscure bug

--- CI1/CCRC/CCRC.ino
+++ CI1/CCRC/CCRC.ino
@@ -7,36 +7,35 @@
 #define DELTA_COM 400
 #define DELTA_PROC 400
 #define TIMESLOT_LEN (DELTA_COM + DELTA_PROC)
-#define INIT_WAIT (3 * TIMESLOT_LEN)
+#define INIT_WAIT (5 * TIMESLOT_LEN)
 #define PAYLOAD_MAX_SIZE 16
 #define GUARD_TIME_BEFORE_TX 15
 
-typedef struct {
+struct payloadHead {
     uint8_t currentSlot;
     uint8_t slotCount;
     uint8_t address;
-} payloadHead;
+};
 
-typedef struct {
-    payloadHead head;
+struct payload {
+    struct payloadHead header;
     uint8_t data[13]; // So total size is 16
-} payload;
+};
 
-typedef struct {
+struct networkStatus {
     uint8_t n; // number of timeslots
     uint8_t k; // the timeslot of this device
     uint8_t i; // the timeslot currently in progress
-} networkStatus;
+};
 
 /* GLOBAL VARIABLES */
 
 RH_ASK rh;
 uint8_t address = 0x0;
-payload inPayload;
-payload outPayload;
+struct payload inPayload;
+struct payload outPayload;
 unsigned long x = 0;
-uint8_t payloadBuffer[PAYLOAD_MAX_SIZE];
-networkStatus netStat; 
+struct networkStatus netStat; 
 bool foundNetwork = false;
 
 /* END GLOBAL VARIABLES */
@@ -63,9 +62,10 @@ void setup() {
     while(getClock(&x) <= INIT_WAIT && !foundNetwork) {
         if(rx()) {
             // FOUND NETWORK
-            if(inPayload.head.slotCount > 1) {
-                netStat.i = inPayload.head.currentSlot;
-                netStat.n = inPayload.head.slotCount;            
+            Serial.println(F("Found Network, joining!!"));
+            if(inPayload.header.slotCount > 1) {
+                netStat.i = inPayload.header.currentSlot;
+                netStat.n = inPayload.header.slotCount;            
                 netStat.k = netStat.n - 1; // EmptySlot, is 0-indexed
                 setPayloadHead(&outPayload, netStat.i,  netStat.n + 1, address);
                 foundNetwork = true;
@@ -80,9 +80,10 @@ void setup() {
         netStat.n = 2;
         netStat.k = 0;
         netStat.i = 1; // Such that when we loop it increments to 
-        outPayload.head.currentSlot = 0;
-        outPayload.head.slotCount = 2;
-        outPayload.head.address = address;
+        outPayload.header.currentSlot = 0;
+        outPayload.header.slotCount = 2;
+        outPayload.header.address = address;
+        Serial.println("Found no network, starting own");
     }
 
     Serial.print(F("slotCount: "));
@@ -123,31 +124,34 @@ void loop() {
     }
 }
 
-int readPayloadFromBuffer(payload* payloadDest, uint8_t plSize) {
-    Serial.print("READ FUCK FACE \t");
-    Serial.println(plSize);
-    Serial.print(payloadBuffer[1]);
-    Serial.print(payloadBuffer[2]);
-    memcpy(payloadDest, payloadBuffer, plSize);
+int readsPayloadFromBuffer(struct payload* payloadDest, uint8_t* payloadBuffer, uint8_t plSize) {
+    payloadDest->header.currentSlot = payloadBuffer[0];
+    payloadDest->header.slotCount = payloadBuffer[1];    
     return 0;
 }
 
-void _printPayload(payload in) {
+void _printPayload(struct payload in) {
     Serial.println(F("Payload:"));
     Serial.print(F("\tcurrentSlot: "));
-    Serial.println(in.head.currentSlot);
+    Serial.println(in.header.currentSlot);
 
     Serial.print(F("\tslotCount: "));
-    Serial.println(in.head.slotCount);
+    Serial.println(in.header.slotCount);
 
     Serial.print(F("\taddress: "));
-    Serial.println(in.head.address);
+    Serial.println(in.header.address);
 }
 
 bool rx() {
-    uint8_t payloadBufferSize = 0;
+    uint8_t payloadBuffer[PAYLOAD_MAX_SIZE];
+    memset(payloadBuffer, 'a', sizeof(payloadBuffer));
+    uint8_t payloadBufferSize = sizeof(payloadBuffer);
     if(rh.recv(payloadBuffer,&payloadBufferSize)) {
-        readPayloadFromBuffer(&inPayload, payloadBufferSize);
+        rh.printBuffer("Got:", payloadBuffer, payloadBufferSize);
+        if(payloadBufferSize == 0) {
+          Serial.println(F("Payload size is zero, wtf"));
+        }
+        readsPayloadFromBuffer(&inPayload, payloadBuffer, payloadBufferSize);
         _printPayload(inPayload);
         return true;
     } else {
@@ -155,12 +159,17 @@ bool rx() {
     }
 }
 
-void writePayloadToBuffer(payload* payloadSrc) {
-    memcpy(payloadBuffer, payloadSrc, PAYLOAD_MAX_SIZE);
+void writePayloadToBuffer(struct payload* payloadSrc) {
+    // memcpy(payloadBuffer, payloadSrc, PAYLOAD_MAX_SIZE);
 }
 
 void tx() {
-    writePayloadToBuffer(&outPayload);
+    //writePayloadToBuffer(&outPayload);
+    uint8_t payloadBuffer[PAYLOAD_MAX_SIZE];
+    memset(payloadBuffer, 'a', sizeof(payloadBuffer));
+    payloadBuffer[0] = outPayload.header.currentSlot;
+    payloadBuffer[1] = outPayload.header.slotCount;
+    payloadBuffer[2] = address;
     rh.send(payloadBuffer, PAYLOAD_MAX_SIZE);
     rh.waitPacketSent();
 }
@@ -173,21 +182,22 @@ void resetClock(unsigned long * x_0) {
     *x_0 = millis();
 }
 
-void setPayloadHead(payload* p, uint8_t curSlot, uint8_t slotCnt, uint8_t addr){
-    p->head.currentSlot = curSlot;
-    p->head.slotCount = slotCnt;
-    p->head.address = addr;  
+void setPayloadHead(struct payload* p, uint8_t curSlot, uint8_t slotCnt, uint8_t addr){
+    p->header.currentSlot = curSlot;
+    p->header.slotCount = slotCnt;
+    p->header.address = addr;  
 }
 
 void nextSlot(){
     netStat.i = (netStat.i + 1) % netStat.n; // This is 0-indexed
-    outPayload.head.currentSlot = netStat.i;  
+    outPayload.header.currentSlot = netStat.i;  
 }
 
 void reSync(){
 	//This is naively assuming that the transmitting device is right
-    netStat.i = inPayload.head.currentSlot;
-    if(inPayload.head.slotCount == 0)
+    netStat.i = inPayload.header.currentSlot;
+    if(inPayload.header.slotCount == 0)
         Serial.println("Something went wrong...");
-    netStat.n = inPayload.head.slotCount;
+    netStat.n = inPayload.header.slotCount;
 }
+
