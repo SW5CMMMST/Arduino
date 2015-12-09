400e7cac0104ca074f2587946c6d80b56cf84165
Troels Kroegh <troels.kroegh@gmail.com>
2015-12-09 14:59:20 +0100

    incorporated doubleclick

--- CI1/CCRC/CCRC.ino
+++ CI1/CCRC/CCRC.ino
@@ -365,6 +365,24 @@ void printTask(const char* mode, unsigned long time){
 }
 #endif
 
+struct doubleClickState {
+  int previousButtonState,
+      buttonState,
+      upDown,
+      sensor;
+  uint32_t doubleClickTimeout,
+           upDownTime;
+};
+
+#define DOUBLE_CLICK_MAX_TIME 750
+#define DEBOUNCE_TIME 25
+
+#define USER_DEBUG
+
+struct doubleClickState dc1 = { -1, 0, HIGH, SENDER_SENSOR_1, 0, 0 };
+struct doubleClickState dc2 = { -1, 0, HIGH, SENDER_SENSOR_2, 0, 0 };
+int outState[4] = { 0, 0, 0, 0 };
+
 /* Place user code which should be executed once pr. timeslot */
 void userCodeRunonce() {
     if(address == SENDER_ADDRESS) {
@@ -374,11 +392,8 @@ void userCodeRunonce() {
         usercodeData[2] = RECEIVER_2_ADDRESS;
         usercodeDataSize = 4;
 
-        // Reset right after transmission
-        if(netStat.i == netStat.k) {
-            usercodeData[1] = LOW;
-            usercodeData[3] = LOW;
-        }
+        usercodeData[1] = outState[SENDER_SENSOR_1];
+        usercodeData[3] = outState[SENDER_SENSOR_2];
 
         userSensorPool();
     } else {
@@ -395,16 +410,69 @@ void userCodeRunonce() {
 /* Place user code which should be executed repeatly here */
 void userCodeRepeat() {
     // Intentionally left blank
+    userSensorPool();
 }
 
 /* Place user code which should be executed repeatly here concurrently while reciving */
 void userSensorPool() {
-    if(digitalRead(SENDER_SENSOR_1) == LOW) {
-        usercodeData[1] = HIGH;
-    }
-
-    if(digitalRead(SENDER_SENSOR_2) == LOW) {
-        usercodeData[3] = HIGH;
-    }
+    checkForDoubleClick(&dc1);
+    checkForDoubleClick(&dc2);
 }
 
+void checkForDoubleClick(struct doubleClickState * doubleClick) {
+  if ((*doubleClick).previousButtonState != (*doubleClick).buttonState) {
+    (*doubleClick).previousButtonState = (*doubleClick).buttonState;
+#ifdef USER_DEBUG
+    Serial.print("Button[");
+    Serial.print((*doubleClick).sensor);
+    Serial.print("] is now: ");
+    Serial.println((*doubleClick).buttonState);
+#endif
+  }
+
+  /* Beck if doubleclick time was exceded  */
+  if ((*doubleClick).buttonState > 1 && (millis() - (*doubleClick).doubleClickTimeout) > DOUBLE_CLICK_MAX_TIME) {
+#ifdef USER_DEBUG
+    Serial.print("Button[");
+    Serial.print((*doubleClick).sensor);
+    Serial.println("] double click was too slow!");
+#endif
+    (*doubleClick).buttonState = 0;
+    (*doubleClick).doubleClickTimeout = millis();
+  }
+
+  /* Button state chanced! with debounce */
+  int temp = digitalRead((*doubleClick).sensor);
+  if (temp != (*doubleClick).upDown && (millis() - (*doubleClick).upDownTime) > DEBOUNCE_TIME) {
+    (*doubleClick).upDown = temp;
+    (*doubleClick).upDownTime = millis();
+    //Serial.println((*doubleClick).upDown ? "NOT PRESSED" : "PRESSED" );
+
+    ((*doubleClick).buttonState)++;
+
+    /* Handles edge case, with invalid state */
+    if ((*doubleClick).upDown == HIGH && (*doubleClick).buttonState % 2 == 1)
+      (*doubleClick).buttonState = 0;
+
+    /* Handles edge case where 2nd button click is held down for too long */
+    if ((*doubleClick).upDown == LOW && (*doubleClick).buttonState % 2 == 0)
+      (*doubleClick).buttonState = 0;
+
+    /* First click is done, start timeout counter */
+    if ((*doubleClick).buttonState == 1) {
+      (*doubleClick).doubleClickTimeout = millis();
+    }
+
+    /* Has been turned off and on again */
+    if ((*doubleClick).buttonState == 4) {
+#ifdef USER_DEBUG
+      Serial.print("Button[");
+      Serial.print((*doubleClick).sensor);
+      Serial.println("] double clicked!");
+#endif
+      (*doubleClick).buttonState = 0;
+      outState[(*doubleClick).sensor] = !outState[(*doubleClick).sensor];
+      digitalWrite(5, outState[(*doubleClick).sensor]);
+    }
+  }
+}
